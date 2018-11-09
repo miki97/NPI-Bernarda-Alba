7e10b20258f05df529e8417883fbc14b12042f83
Jaime Frías <jaimefrias_76@hotmail.com>
2018-11-09 18:53:55 +0100

    walkthrough

--- app/src/main/java/conversandroid/Welcome.java
+++ app/src/main/java/conversandroid/Welcome.java
@@ -1,22 +1,60 @@
 package conversandroid;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
-import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.shashank.sony.fancywalkthroughlib.FancyWalkthroughActivity;
+import com.shashank.sony.fancywalkthroughlib.FancyWalkthroughCard;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import conversandroid.chatbot.R;
 
-public class Welcome extends AppCompatActivity {
-    private ViewPager mSlideViewPager;
-    private LinearLayout mDotLayout;
+public class Welcome extends FancyWalkthroughActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        FancyWalkthroughCard fancywalkthroughCard1 = new FancyWalkthroughCard("Bernarda alba", "App para museo",R.drawable.conversandroid);
+        FancyWalkthroughCard fancywalkthroughCard2 = new FancyWalkthroughCard("Pick the best", "Pick the right place using trusted ratings and reviews.",R.drawable.microphone_control);
+        FancyWalkthroughCard fancywalkthroughCard3 = new FancyWalkthroughCard("Choose your meal", "Easily find the type of food you're craving.",R.drawable.bernarda);
+        FancyWalkthroughCard fancywalkthroughCard4 = new FancyWalkthroughCard("Meal is on the way", "Get ready and comfortable while our biker bring your meal at your door.",R.drawable.bernarda);
 
-        mSlideViewPager = (ViewPager) findViewById(R.id.slideViewPager);
-        mDotLayout = (LinearLayout) findViewById(R.id.dotsLayout);
+        fancywalkthroughCard1.setBackgroundColor(R.color.white);
+        fancywalkthroughCard1.setIconLayoutParams(300,300,0,0,0,0);
+        fancywalkthroughCard2.setBackgroundColor(R.color.white);
+        fancywalkthroughCard2.setIconLayoutParams(300,300,0,0,0,0);
+        fancywalkthroughCard3.setBackgroundColor(R.color.white);
+        fancywalkthroughCard3.setIconLayoutParams(300,300,0,0,0,0);
+        fancywalkthroughCard4.setBackgroundColor(R.color.white);
+        fancywalkthroughCard4.setIconLayoutParams(300,300,0,0,0,0);
+        List<FancyWalkthroughCard> pages = new ArrayList<>();
+
+        pages.add(fancywalkthroughCard1);
+        pages.add(fancywalkthroughCard2);
+        pages.add(fancywalkthroughCard3);
+        pages.add(fancywalkthroughCard4);
+
+        for (FancyWalkthroughCard page : pages) {
+            page.setTitleColor(R.color.black);
+            page.setDescriptionColor(R.color.black);
+        }
+        setFinishButtonTitle("Get Started");
+        showNavigationControls(true);
+        setColorBackground(R.color.fab_material_blue_900);
+        //setImageBackground(R.drawable.restaurant);
+        setInactiveIndicatorColor(R.color.grey_600);
+        setActiveIndicatorColor(R.color.fab_material_blue_900);
+        setOnboardPages(pages);
+
+    }
+
+    @Override
+    public void onFinishButtonPressed() {
+        Intent intent = new Intent(getBaseContext(), MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
