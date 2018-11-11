74f1c867b6abd7eced42d3a8ca76300d43287475
Jaime Frías <jaimefrias_76@hotmail.com>
2018-11-11 13:42:57 +0100

    Añadidas preguntas de ejemplo y iconito

--- app/src/main/java/conversandroid/que_preguntar.java
+++ app/src/main/java/conversandroid/que_preguntar.java
@@ -1,8 +1,12 @@
 package conversandroid;
 
 
+import android.app.ActionBar;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.widget.Toolbar;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,13 +15,18 @@ import conversandroid.chatbot.R;
 
 public class que_preguntar  extends AppCompatActivity {
 
+    Toolbar barraSuperior;
+
+    int uiOptions;
+    View decorView;
+    ActionBar actionBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.que_preguntar);
-
-
-
+        barraSuperior = (Toolbar) findViewById(R.id.toolbar);
+        //barraSuperior.setBackgroundColor(getResources().getColor(R.color.lightBar));
     }
 
 }
